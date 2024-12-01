943674c7ee4f4981b2a3422993fc7200c7d27311
junevision <junevision@163.com>
2024-12-01 18:44:30 +0800

    create event manage html and endpoint for redirect

--- sign/views.py
+++ sign/views.py
@@ -1,15 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
+def event_manage(request):
+    return render(request, "event_manage.html")
+
 def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         if username == 'admin' and password == 'admin123':
-            return HttpResponse('Login success!')
+            return HttpResponseRedirect('/event_manage/')
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
