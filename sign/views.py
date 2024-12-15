bf2d479ce0f046c32a020f91d7564a0ce8fca962
JUN <1012762794@qq.com>
2024-12-15 19:16:33 +0800

    enable django authentication

--- sign/views.py
+++ sign/views.py
@@ -1,3 +1,5 @@
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -7,8 +9,8 @@ def index(request):
     return render(request, "index.html")
 
 
+@login_required
 def event_manage(request):
-    # username = request.COOKIES.get('user', '')  # get cookie
     username = request.session.get('user', '')  # get session from browser
     return render(request, "event_manage.html", {"user": username})
 
@@ -17,10 +19,11 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
-        if username == 'admin' and password == 'admin123':
-            response = HttpResponseRedirect('/event_manage/')
-            # response.set_cookie('user', username, 3600)  # add browser cookie
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)  # login
             request.session['user'] = username  # add session to browser
+            response = HttpResponseRedirect('/event_manage/')
             return response
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
