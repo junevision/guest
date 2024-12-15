7d797400b7a868e299a93b666929c82f9c46ff9d
JUN <1012762794@qq.com>
2024-12-15 18:43:42 +0800

    use session instead of cookie

--- sign/views.py
+++ sign/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
+
 def event_manage(request):
-    username = request.COOKIES.get('user', '')  # get cookie
+    # username = request.COOKIES.get('user', '')  # get cookie
+    username = request.session.get('user', '')  # get session from browser
     return render(request, "event_manage.html", {"user": username})
+
 
 def login_action(request):
     if request.method == 'POST':
@@ -15,7 +19,8 @@ def login_action(request):
         password = request.POST.get('password', '')
         if username == 'admin' and password == 'admin123':
             response = HttpResponseRedirect('/event_manage/')
-            response.set_cookie('user', username, 3600)  # add browser cookie
+            # response.set_cookie('user', username, 3600)  # add browser cookie
+            request.session['user'] = username  # add session to browser
             return response
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
